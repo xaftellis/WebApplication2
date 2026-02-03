895b15af4094f30717be21aa8bce9141dc477c86
Xaftellis <xaftellis@outlook.com>
2026-02-03 14:38:40 +0800

    03.02.2026.1

--- WebApplication2/Controllers/StoreController.cs
+++ WebApplication2/Controllers/StoreController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using WebApplication2.Models;
+using System.Drawing;
 
 namespace WebApplication2.Controllers
 {
     public class StoreController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public StoreController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -13,5 +24,23 @@ namespace WebApplication2.Controllers
         {
             return View();
         }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create(Product model)
+        //{
+        //    if (!ModelState.IsValid) return View(model);
+
+        //    if (model.Photo != null && model.Photo.Length > 0)
+        //    {
+        //        using var ms = new MemoryStream();
+        //    }
+
+        //    _context.Products.Add(model);
+        //    await _context.SaveChangesAsync();
+
+        //    return RedirectToAction(nameof(Index));
+        //}
+
     }
 }
